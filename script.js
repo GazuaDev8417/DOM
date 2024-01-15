33f67a0e64f6f0787da049d02cea4951ab609597
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2024-01-15 18:52:52 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const span = document.createElement('span')
 const main = document.createElement('main')
 const footer = document.createElement('footer')
 
+
 /* Instance of body */
 const body = document.body
 
@@ -17,110 +18,126 @@ header.innerHTML = `<h1>Manipulando DOM</h1>`
 span.setAttribute('class', 'logout')
 span.innerHTML = `${!logged ? '<span class="loggedout-text">Você não está logado</span>' :  'Logout'}`
 main.setAttribute('class', 'middle')
+footer.setAttribute('class', 'footer-text')
 
-
-/* Displaying elements on screen */
-main.innerHTML = `
-    ${!logged ? (
-        '<h2>Tela de login usando manipulação de DOM por javascript</h2>'
-    ) : `<h2>${user.hisName} está logado(a)`}
-`
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    if(user && !logged){
-        main.innerHTML += `
-            <form id="form">
-            <fieldset>
-                <legend>${user ? 'Login' : 'Registro'}</legend>
-                ${!user ? (
-                    `<input 
-                    type="text"
-                    id="yourName"
-                    class="input"
-                    placeholder='Seu nome'
-                    required>`
-                ) : ''}
-                <input 
-                    type="email"
-                    id="email"
-                    class="input"
-                    placeholder='E-mail'
-                    required>
-                <div class="eye-container">
-                    <input 
-                        type="password"
-                        id="keyword"
+    /* Displaying elements on screen */
+    body.appendChild(header)
+    body.appendChild(main)
+    header.appendChild(span)
+
+
+    main.innerHTML += `
+        ${!logged ? (
+            '<h2>Tela de login usando manipulação de DOM por javascript</h2>'
+        ) : `<h2>${user.hisName} está logado(a)</h2>`}
+    `
+    main.innerHTML += `
+        ${!logged ? (
+            `<form id="form">
+                <fieldset>
+                    <legend>${user ? 'Login' : 'Registro'}</legend>
+                    ${!user ? (
+                        `<input 
+                        type="text"
+                        id="yourName"
                         class="input"
-                        placeholder='Sua senha'
+                        placeholder='Seu nome'
+                        required>`
+                    ) : ''}
+                    <input 
+                        type="email"
+                        id="email"
+                        class="input"
+                        placeholder='E-mail'
                         required>
-                    <div class="eye">Mostrar</div>
-                </div>
-                <div class="btn-container">
-                    <button type="reset">Limpar</button>
-                    <button type="submit">Enviar</button>
-                </div>
-            </fieldset>
-            </form>
-        `
-    
-        /* Form variables */
-        const yourName = document.querySelector('#yourName')
-        const email = document.querySelector('#email')
-        const keyword = document.querySelector('#keyword')
-        const eye = document.querySelector('.eye')
-    
-        /* Show and hide password */
-        eye.addEventListener('click', ()=>{
-            if(keyword.type === 'password'){
-                keyword.setAttribute('type', 'text')
-                eye.innerText = 'Ocultar'
-            }else{
-                keyword.setAttribute('type', 'password')
-                eye.innerText = 'Mostrar'
+                    <div class="eye-container">
+                        <input 
+                            type="password"
+                            id="keyword"
+                            class="input"
+                            placeholder='Sua senha'
+                            required>
+                        <div class="eye">Mostrar</div>
+                    </div>
+                    <div class="btn-container">
+                        <button type="reset">Limpar</button>
+                        <button type="submit">Enviar</button>
+                    </div>
+                </fieldset>
+            </form>`
+        ) : ''}
+    `
+
+    /* Form variables */
+    const yourName = document.querySelector('#yourName')
+    const email = document.querySelector('#email')
+    const keyword = document.querySelector('#keyword')
+    const eye = document.querySelector('.eye')
+
+    /* Show and hide password */
+    eye.addEventListener('click', ()=>{
+        if(keyword.type === 'password'){
+            keyword.setAttribute('type', 'text')
+            eye.innerText = 'Ocultar'
+        }else{
+            keyword.setAttribute('type', 'password')
+            eye.innerText = 'Mostrar'
+        }
+    })
+
+    /* Submit form */
+    document.querySelector('#form').addEventListener('submit', (e)=>{
+        e.preventDefault()
+        if(!user){
+            const userData = {
+                hisName: yourName.value,
+                email: email.value,
+                keyword: keyword.value
             }
-        })
     
-        /* Submit form */
-        document.querySelector('#form').addEventListener('submit', (e)=>{
-            e.preventDefault()
-            if(!user){
-                const userData = {
-                    hisName: yourName.value,
-                    email: email.value,
-                    keyword: keyword.value
-                }
-        
-                if(userData.keyword.length < 6){
-                    alert('Sua senha deve ter no mínimo 6 caracteres')
-                    return
-                }
-    
-                localStorage.setItem('user', JSON.stringify(userData))
+            if(userData.keyword.length < 6){
+                alert('Sua senha deve ter no mínimo 6 caracteres')
+                return
+            }
+
+            localStorage.setItem('user', JSON.stringify(userData))
+            localStorage.setItem('logged', 'logged')
+            location.reload()
+        }else{
+            if(email.value !== user.email || keyword.value !== user.keyword){
+                alert('Usuário não encontrado!')
+            }else{
+                localStorage.setItem('logged', 'logged')
                 location.reload()
-            }else{
-                if(email.value !== user.email || keyword.value !== user.keyword){
-                    alert('Usuário não encontrado!')
-                }else{
-                    localStorage.setItem('logged', 'logged')
-                    location.reload()
-                }
             }
-    
-        })
-    }
+        }
+    })
+
+    main.appendChild(footer).innerHTML = `
+        <div class="footer-text">
+            <p>
+                Copyright &copy; 2023 por 
+                <a href="https://portfolio-x22d.onrender.com/" target="_blank">
+                    Flamarion França
+                </a>
+            </p>
+        </div>
+    `
 })
-
-body.appendChild(header)
-body.appendChild(main)
-header.appendChild(span)
-
+    
 /* logout */
 span.addEventListener('click', ()=>{
-    const decide = window.confirm('Tem certeza que quer deslogar?')
-    if(decide){
-        localStorage.removeItem('logged')
-        location.reload()
+    if(logged){
+        const decide = window.confirm('Tem certeza que quer deslogar?')
+        if(decide){
+            localStorage.removeItem('logged')
+            location.reload()
+        }
     }
 })
+
+
 
 
